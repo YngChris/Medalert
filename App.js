a15f587f1b7298f93013ee101fa2fe1ca82ef7ef
thelocalgodd <splashvincentk@outlook.com>
2025-08-06 14:26:20 +0000

    aaa

--- App.js
+++ App.js
@@ -1,44 +1,94 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './LoginScreen';
-import SignupScreen from './SignupScreen';
-import ForgotPasswordScreen from './ForgotPasswordScreen';
-import HomeScreen from './HomeScreen';
-import SettingsScreen from './SettingsScreen';
-import { ReportformScreen } from './ReportformScreen';
-import { LocationsScreen } from './LocationsScreen';
-import { AlertsScreen } from './AlertsScreen';
-import { EducationScreen } from './EducationScreen';
-import { ForumScreen } from './ForumScreen';
-import { MyReportsScreen } from './MyReportsScreen'; 
-import { RecycleBinScreen } from './RecycleBinScreen'; 
-import ProfileScreen from './ProfileScreen';
-import EditProfileScreen from './EditProfileScreen';
-
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./src/screens/LoginScreen";
+import SignupScreen from "./src/screens/SignupScreen";
+import ForgotPasswordScreen from "./src/screens/ForgotPasswordScreen";
+import HomeScreen from "./src/screens/HomeScreen";
+import SettingsScreen from "./src/screens/SettingsScreen";
+import { ReportformScreen } from "./src/screens/ReportformScreen";
+import { LocationsScreen } from "./src/screens/LocationsScreen";
+import { AlertsScreen } from "./src/screens/AlertsScreen";
+import { EducationScreen } from "./src/screens/EducationScreen";
+import { ForumScreen } from "./src/screens/ForumScreen";
+import { MyReportsScreen } from "./src/screens/MyReportsScreen";
+import { RecycleBinScreen } from "./src/screens/RecycleBinScreen";
+import ProfileScreen from "./src/screens/ProfileScreen";
+import EditProfileScreen from "./src/screens/EditProfileScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return ( 
+  return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Signup" component={SignupScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Settings" component={SettingsScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Report" component={MyReportsScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="ReportForm" component={ReportformScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Locations" component={LocationsScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Alerts" component={AlertsScreen} options={{ headershown: false }} />
-        <Stack.Screen name="Education" component={EducationScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Forum" component={ForumScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="RecycleBin" component={RecycleBinScreen} options={{ headerShown: false }}/>
-        <Stack.Screen name="Profile" component={ProfileScreen} options={{ title: 'Profile', headerBackTitle: 'Back' }}/>
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Signup"
+          component={SignupScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="ForgotPassword"
+          component={ForgotPasswordScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Settings"
+          component={SettingsScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Report"
+          component={MyReportsScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="ReportForm"
+          component={ReportformScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Locations"
+          component={LocationsScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Alerts"
+          component={AlertsScreen}
+          options={{ headershown: false }}
+        />
+        <Stack.Screen
+          name="Education"
+          component={EducationScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Forum"
+          component={ForumScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="RecycleBin"
+          component={RecycleBinScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{ title: "Profile", headerBackTitle: "Back" }}
+        />
         <Stack.Screen name="EditProfile" component={EditProfileScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
- 
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,15 +1,15 @@
 // firebase.js
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
- apiKey: "AIzaSyBHlyWUAVH0b9Nh88rg6i-OgP_AUczt240",
+  apiKey: "AIzaSyBHlyWUAVH0b9Nh88rg6i-OgP_AUczt240",
   authDomain: "medalert-3557a.firebaseapp.com",
   projectId: "medalert-3557a",
   storageBucket: "medalert-3557a.firebasestorage.app",
   messagingSenderId: "899645498850",
   appId: "1:899645498850:web:a10c2f82eaa830282e05bb",
-  measurementId: "G-R09P1D51SB"
+  measurementId: "G-R09P1D51SB",
 };
 
 const app = initializeApp(firebaseConfig);
